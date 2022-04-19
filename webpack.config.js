5bd20ebbb12fab733c9c66ac93ddc04c39beb056
dreamthen <dreamthen.00@gmail.com>
2022-04-19 17:01:22 +0800

    build:构建html-webpack-plugin压缩提取html并导入js、css文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 var path = require('path');
 var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 var CSSMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
-var {CleanWebpackPlugin} = require("clean-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 var ENTRY_PATH = path.join(__dirname, 'index.js');
 var OUTPUT_PATH = path.join(__dirname, 'dist');
+var PUBLIC_PATH = '.';
+var TEMPLATE_PATH = path.join(__dirname, 'index.html');
 var MODE = process.env.MODE;
 
 var webpackConfig = {
@@ -32,6 +35,14 @@ var webpackConfig = {
 		new CleanWebpackPlugin(),
 		new MiniCssExtractPlugin({
 			filename: '[name].[contenthash:6].css'
+		}),
+		new HtmlWebpackPlugin({
+			publicPath: PUBLIC_PATH,
+			filename: 'index.html',
+			template: TEMPLATE_PATH,
+			inject: 'body',
+			chunks: ['index'],
+			minify: true
 		})
 	]
 };
