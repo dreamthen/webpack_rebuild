56fe666928d0ae198528d0e662b3489d1e055b3f
dreamthen <dreamthen.00@gmail.com>
2022-04-19 16:43:03 +0800

    build:构建mini-css-extract-plugin and css-minimizer-webpack-plugin压缩提取css文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,39 @@
 var path = require('path');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var CSSMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+var {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 var ENTRY_PATH = path.join(__dirname, 'index.js');
 var OUTPUT_PATH = path.join(__dirname, 'dist');
 var MODE = process.env.MODE;
 
 var webpackConfig = {
-	entry: ENTRY_PATH,
+	entry: {
+		index: ENTRY_PATH
+	},
 	output: {
 		path: OUTPUT_PATH,
-		filename: 'index.js'
+		filename: '[name].[fullhash].js'
 	},
 	mode: MODE,
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new CSSMinimizerWebpackPlugin()
+		]
+	},
 	module: {
 		rules: [{
 			test: /\.css$/,
-			use: [{loader: 'style-loader'}, {loader: 'css-loader'}]
+			use: [MiniCssExtractPlugin.loader, {loader: 'css-loader'}]
 		}]
-	}
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({
+			filename: '[name].[contenthash:6].css'
+		})
+	]
 };
 
 module.exports = webpackConfig;
